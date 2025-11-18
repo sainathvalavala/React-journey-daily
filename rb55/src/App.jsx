8329aa4e8e1402d3a810c472037b21d03ead27e9
Sainathgit config --global user.name Sainathgit config --global user.name Sainath <sainath.valavala.31@gmail.com>
2025-11-18 14:58:22 +0530

    updated

---
+++ rb55/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+
+function App() {
+  return (
+    <>
+     <div>App</div>
+     <Navbar></Navbar>
+    </>
+   
+  )
+}
+
+export default App
